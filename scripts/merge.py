f6262cd52fa9e147ecf91f8a02a712dd17859178
Gaurav Vaidya <gaurav@ggvaidya.com>
2022-05-02 02:32:58 -0400

    First stab at a merge script.

--- scripts/merge.py
+++ scripts/merge.py
@@ -5,7 +5,7 @@
 #   - By default, we merge entries from all files; entries having the same
 #     source_url are assumed to be identical and their tracks will be merged
 #     rather than being duplicated.
-#   - However, you can also turn on `--annotate-first` mode, which will only
+#   - However, you can also turn on `--filter-to-first` mode, which will only
 #     add tracks to the first file given.
 
 import logging
@@ -19,20 +19,68 @@ import requests
 
 logging.basicConfig(level=logging.INFO)
 
-def merge_file(input_path, output_file, kwargs):
+def merge_file(input_path, output_path):
     """
     Merge an individual file into an output using the criteria provided.
 
     :param input_path: Input file path
     :param output_file: Output file path
-    :param kwargs: configuration parameters from the command line
     """
 
     with open(input_path) as input:
         for line in input:
             entry = json.loads(line)
 
+            # Figure out the PMID
+            source_url = entry['source_url']
+            if not source_url.startswith('https://pubmed.ncbi.nlm.nih.gov/'):
+                raise RuntimeError(f"Sorry, only PubMed abstracts currently supported; source URL '{source_url}' cannot be parsed.")
 
+            pmid = source_url[32:]
+            if pmid.endswith('/'):
+                pmid = pmid[:-1]
+
+            logging.info(f"Loaded entry with PMID {pmid}")
+
+            # If this file doesn't already exist in the output directory,
+            # then this is easy: we only need to copy it over.
+            output_file = os.path.join(output_path, f"pmid-{pmid}.json")
+            if not os.path.exists(output_file) or os.path.getsize(output_file) == 0:
+                with open(output_file, 'w') as f:
+                    json.dump(entry, f, sort_keys=True)
+                logging.info(f"Copied entry to {output_file}")
+
+            else:
+                # Otherwise, we need to merge our entry with the provided entry.
+                with open(output_file, 'r') as fin:
+                    merge_with = json.load(fin)
+
+                logging.info(f"Merging entry with existing {output_file}")
+
+                # Make sure the source_url and codes are identical.
+                assert source_url == merge_with['source_url']
+                assert entry['text'] == merge_with['text']
+
+                # Compare tracks.
+                tracks = entry['tracks']
+                merge_with_tracks = merge_with['tracks']
+                merge_with_tracks_projects = set(map(lambda t: t['project'], merge_with_tracks))
+
+                flag_modified = False
+                for track in tracks:
+                    project = track['project']
+
+                    if project in merge_with_tracks_projects:
+                        logging.warning(f"Track '{project}' already exists, not merging.")
+                    else:
+                        merge_with['tracks'].append(track)
+                        logging.info(f"Adding track '{project}', not present in the original.")
+                        flag_modified = True
+
+                if flag_modified:
+                    logging.info(f"Updated entry, writing back to {output_file}")
+                    with open(output_file, 'w') as f:
+                        json.dump(entry, f, sort_keys=True)
 
 @click.command()
 @click.argument('input', nargs=-1, type=click.Path(
@@ -40,12 +88,17 @@ def merge_file(input_path, output_file, kwargs):
     dir_okay=True,
     exists=True
 ))
-@click.option('--output', '-O', default='-', type=click.File('w'))
-@click.option('--annotate-first', type=bool)
+@click.option('--output', '-O', required=True, type=click.Path(
+    file_okay=False,
+    dir_okay=True,
+    exists=False
+))
 def merge(input, output, **kwargs):
     """
     merge.py [files or directories containing JSONL files to merge]
     """
+    output_path = click.format_filename(output)
+
     for inp in input:
         input_path = click.format_filename(inp)
 
@@ -53,7 +106,7 @@ def merge(input, output, **kwargs):
             # TODO: make this better.
             logging.info(f"Globbing: {f'{input_path}/**/*.jsonl'}.")
             for filename in glob.iglob(f'{input_path}/**/*.jsonl', recursive=True):
-                merge_file(filename, output, kwargs)
+                merge_file(filename, output_path)
         else:
             merge_file(input_path, output, kwargs)
 
